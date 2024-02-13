7f945e079d2119a62199b946ff4ba49dcfedc39b
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-02-13 12:24:01 +0100

    0.5

---
+++ src/scripts/controllers/botController.js
@@ -0,0 +1,34 @@
+import BotModel from '../models/botModel.js';
+import BotView from '../views/botView.js';
+
+export default class BotController {
+  constructor(botModel, botView) {
+    this.botModel = botModel;
+    this.botView = botView;
+
+    this.addDefaultBots();
+  }
+
+  addDefaultBots() {
+    const defaultsBotsData = [
+      { name: 'Bot 1', actions: ['action1', 'action2'] },
+      { name: 'Bot 2', actions: ['action1', 'action2'] },
+      { name: 'Bot 3', actions: ['action1', 'action2'] }
+    ];
+
+    defaultsBotsData.forEach(botData => {
+      const newBot = new BotModel(botData.name, botData.actions);
+      this.botModel.addBot(newBot);
+    });
+  }
+
+  interactionWithBot(botName, action) {
+    const bot = this.botModel.getBotByName(botName);
+    if (bot) {
+      const response = bot.processAction(action);
+      this.botView.displayResponse(botName, response);
+    } else {
+      this.botView.displayError(`Bot "${botName}" not found.`);
+    }
+  }
+}
