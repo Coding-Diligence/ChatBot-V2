d59a4c02bacbbe8ecd86ae24f36534abf5ae7557
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-03-25 10:47:57 +0100

    0.25

--- src/scripts/views/render.js
+++ src/scripts/views/render.js
@@ -1,5 +1,5 @@
 import renderBots from './botView';
-import { renderConversations } from './chatView';
+import './chatView.php';
 import messageHandler from './messageHandler';
 
 const fullName = ['John', 'Emma', 'Michael', 'Sophia', 'William', 'Isabella', 'James', 'Olivia', 'Benjamin', 'Amelia', 'Ethan', 'Ava', 'Matthew', 'Charlotte', 'Daniel', 'Sophie'];
